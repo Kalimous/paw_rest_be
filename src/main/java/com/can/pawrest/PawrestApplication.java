9b7ca502c94e5c10c34ee4ebe416e30e8ef63e7c
Kalimous <elsword485667@gmail.com>
2025-01-12 01:39:01 +0900

    first commit

---
+++ src/main/java/com/can/pawrest/PawrestApplication.java
@@ -0,0 +1,13 @@
+package com.can.pawrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawrestApplication.class, args);
+	}
+
+}
